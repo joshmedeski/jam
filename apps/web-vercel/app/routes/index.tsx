35fe07a282584c457332282aef449c4c210b387d
Josh Medeski <josh.medeski@gmail.com>
2022-05-26 10:50:14 -0500

    feat: add cloudflare pages

---
+++ apps/web-vercel/app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Josh Medeski</h1>
+      <p>Site coming soon.</p>
+    </div>
+  );
+}
--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -2,14 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  serverBuildTarget: "cloudflare-pages",
+  server: "./server.js",
+  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "api/index.js",
+  // serverBuildPath: "functions/[[path]].js",
   // publicPath: "/build/",
 };
